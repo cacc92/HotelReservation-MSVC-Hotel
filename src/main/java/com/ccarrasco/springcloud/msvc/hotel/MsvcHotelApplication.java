b1d965659673d344dd52ea5221cb867b8f65a6ac
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-19 15:57:22 -0400

    Init Commit

---
+++ src/main/java/com/ccarrasco/springcloud/msvc/hotel/MsvcHotelApplication.java
@@ -0,0 +1,13 @@
+package com.ccarrasco.springcloud.msvc.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcHotelApplication.class, args);
+	}
+
+}
