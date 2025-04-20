754636759b7e2052624568fb0daf75b892ee25da
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-19 20:47:07 -0400

    Se generan los comnetarios del controller

--- src/main/java/com/ccarrasco/springcloud/msvc/hotel/controllers/HotelController.java
+++ src/main/java/com/ccarrasco/springcloud/msvc/hotel/controllers/HotelController.java
@@ -12,13 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * @RestController -> Nos permite realizar un controlador del tipo REST, recordar que el controlador el puerta de
+ * entrada y salida de la información de nuestra aplicación
+ * @RequestMapping -> Es el elemento que nos permite definir el Path o URL padre de los endpoints que maneja el
+ * controlador.
+ */
 @RestController
 @RequestMapping("/api/v1/hotels")
 public class HotelController {
 
+
+    /**
+     * Se genera con la anotacion @Autowired la conexion entre el repositorio y el controlador, ojo al momenot de llamar
+     * al service se llama la interface del service dado que el controller no necesita saber como esta implementado el
+     * servicio.
+     */
     @Autowired
     private HotelRepository hotelRepository;
 
+    /**
+     *  Se genera un get mapping para obtener el listado de los hoteles
+     *  Recordar que el ResponseEntity contiene el codigo de resultado de la transaccción, el header y el body (el cual
+     *  seria la data que nosotros le devolvemos)
+     * @return
+     */
     @GetMapping
     public ResponseEntity<List<Hotel>> getAllHotels() {
         List<Hotel> hotels = hotelRepository.findAll();
